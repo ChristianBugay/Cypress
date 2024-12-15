0807a4d90754e623e7faed0ce4485cef6cf31540
Christian PC <christianbugay@hotmail.com>
2024-12-15 00:52:10 -0300

    New features

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,40 +1,25 @@
-import {faker} from '@faker-js/faker'
-
-export function generateUser() {
-    let person = new Object();
-    person.user = faker.person.firstName();
-    person.email = faker.internet.email();
-    person.password = faker.internet.password();
-    return person;
-}
-
-Cypress.Commands.add('signIn', (person) => {
-    cy.get('[data-testid="cadastrar"]').click()
-    cy.get('[data-testid="nome"]').should('be.visible').type(person.user)
-    cy.get('[data-testid="email"]').should('be.visible').type(person.email)
-    cy.get('[data-testid="password"]').should('be.visible').type(person.password)
-    cy.get('[data-testid="checkbox"]').click()
-    cy.get('[data-testid="cadastrar"]').click()
-    cy.get('.alert-link').should('be.visible').should('contain', 'Cadastro realizado com sucesso')
-    cy.contains('Bem Vindo ' + person.user)
-})
-
-Cypress.Commands.add('login', (email, password, name) => {
-    cy.get('[data-testid="email"]').type(email)
-    cy.get('[data-testid="senha"]').type(password)
-    cy.get('[data-testid="entrar"]').should('be.visible').click()
-    cy.contains('Entrar').click()
-    cy.contains('Bem Vindo ' + name).should('be.visible')
-})
-
-Cypress.Commands.add('registerNewUser', (newUser) => {
-    cy.contains('Cadastrar Usuários').click()
-    cy.get('[data-testid="nome"]').should('be.visible').type(newUser.user)
-    cy.get('[data-testid="email"]').should('be.visible').type(newUser.email)
-    cy.get('[data-testid="password"]').should('be.visible').type(newUser.password)
-    cy.get('[data-testid="cadastrarUsuario"]').click()
-    cy.contains('Lista dos usuários', { timeout: 5000 }).should('be.visible')
-    cy.contains(newUser.user).should('be.visible')
-    cy.contains(newUser.email).should('be.visible')
-    cy.contains(newUser.password).should('be.visible')
-})
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
